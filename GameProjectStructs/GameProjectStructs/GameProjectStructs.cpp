6787d89ba3a42f0b7a6d68b85f34a982b1bfe948
snkolev18 <SNKolev@codingburgas.bg>
2020-04-02 01:47:18 +0300

    Function

--- GameProjectStructs/GameProjectStructs/GameProjectStructs.cpp
+++ GameProjectStructs/GameProjectStructs/GameProjectStructs.cpp
@@ -1,20 +1,53 @@
-// GameProjectStructs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct GAMESHOP {
+	string nameGame = "";
+	float price = 0.000000000;
+	int year = 0;
+	int sales = 0;
+	string company = "";
+	int gameSize = 0;
+	string reviews = "";
+	int gameCount = 0;
+};
+
+void addGame(GAMESHOP game[], int& gameCount) {
+	cout << "Enter the name of game: ";
+	cin >> game[gameCount].nameGame;
+	cout << endl;
+	cout << "Enter the price of the game: ";
+	cin >> game[gameCount].price;
+	cout << endl;
+	cout << "Enter the publish year: ";
+	cin >> game[gameCount].year;
+	cout << endl;
+	cout << "Enter how many copies have been sold: ";
+	cin >> game[gameCount].sales;
+	cout << endl;
+	cout << "Enter the name of the company creator: ";
+	cin >> game[gameCount].company;
+	cout << endl;
+	cout << "Enter the size of the game: ";
+	cin >> game[gameCount].gameSize;
+	cout << endl;
+	cout << "Enter the review of the game: ";
+	cin >> game[gameCount].reviews;
+	cout << endl;
+
+	gameCount++;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//******************************
+
+
+//***************PresentationLayer***************
+
+//Code
+
+//******************************
+
+int main() {
+
+}
