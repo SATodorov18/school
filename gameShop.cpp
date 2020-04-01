47113d6ac8de331019de320e8ed877b98513133f
snkolev18 <SNKolev@codingburgas.bg>
2020-04-01 19:57:33 +0300

    add .cpp file and write the struct in it

---
+++ gameShop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct GAMESHOP {
+	string nameGame = "";
+	float price;
+	int year;
+	int sales = 0;
+	string company = "";
+	int gameSize = 0;
+	string reviews = "";
+};
+
+int main() {
+	
+}
